ff55d4566e2bbbbd326645810f2138b11bad2620
wstlabs <notyet@notyet.org>
2017-02-16 15:48:30 -0500

    module comments

--- shapeutil/__init__.py
+++ shapeutil/__init__.py
@@ -1,3 +1,14 @@
+"""
+Provides a slightly simplified (and theoretically, generic) interface to
+the standard shapefile library, to make our parsing tasks somewhat easier.
+
+Or at least, mostly generic (as applies to the 'core' and 'extras' modules).
+The 'nycgis' module contains a small, but critical coordinate projection function
+which (as the name implies) is specific to NYC-based GIS calculations.  In principle,
+this function should go under some other namespace (so as to keep the functionality
+of this package strictly generic).  But for the time being we'll just accept that
+piece of messines, so that we can get on with the work we need to do.
+"""
 import shapefile as sf
 from . import core
 
