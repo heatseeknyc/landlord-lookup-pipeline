c7b1f98a25e5a9626c1fdbff0899c1309fcbe615
wstlabs <notyet@notyet.org>
2017-02-23 12:03:39 -0500

    refactor pluto unpack

--- bin/unpack-pluto.sh
+++ bin/unpack-pluto.sh
@@ -1,14 +1,19 @@
 #!/bin/bash -ue
 #
-# Unpack the Pluto zip, and get rid of the unsighly intermediate directory.
-# Specific to 16v2.  As with all things Pluto, the name (or presence of) this 
-# intermediate directory may well change in future versions, and so this script
+# Unpack just the shapefile data files from the 5 mappluto zips.  (Note that 
+# this corresponds to the set of files with extensions of exactly 3 lower case 
+# letters, per the globbing pattern below).
+#
+# Specific to 16v2.  As with all things Pluto, the layout and naming conventions 
+# for these files may well change in future Pluto releases, and so this script
 # will need to be refactored accordingly.
 #
 
+ver='16v2'
 destdir='stage/unpack/pluto'
-tempdir='stage/unpack/pluto-temp'
-echo "pluto to $destdir/ .."
-unzip -q stage/incoming/pluto.zip '*.csv' -d $tempdir 
-mv $tempdir/BORO_zip_files_csv $destdir 
-rm -r $tempdir 
+echo "mappluto $ver to $destdir/ .."
+unzip -q stage/incoming/pluto/mn_mappluto_$ver.zip 'MNMapPLUTO.[a-z][a-z][[a-z]' -d $destdir 
+unzip -q stage/incoming/pluto/bk_mappluto_$ver.zip 'BKMapPLUTO.[a-z][a-z][[a-z]' -d $destdir 
+unzip -q stage/incoming/pluto/bx_mappluto_$ver.zip 'BXMapPLUTO.[a-z][a-z][[a-z]' -d $destdir 
+unzip -q stage/incoming/pluto/qn_mappluto_$ver.zip 'QNMapPLUTO.[a-z][a-z][[a-z]' -d $destdir 
+unzip -q stage/incoming/pluto/si_mappluto_$ver.zip 'SIMapPLUTO.[a-z][a-z][[a-z]' -d $destdir 
