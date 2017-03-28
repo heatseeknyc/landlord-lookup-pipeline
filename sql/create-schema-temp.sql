f50971f37aba7cdd248156f9f43ee88353d25f25
wstlabs <notyet@notyet.org>
2017-03-28 15:12:44 -0400

    new temp schema w/ provisional new taxbills table

---
+++ sql/create-schema-temp.sql
@@ -0,0 +1,20 @@
+--
+-- A new side schema for provisional designs of new tables in the 'flat' schema. 
+--  
+
+begin;
+
+drop schema if exists temp cascade;
+create schema temp; 
+
+create table temp.taxbills (
+    bbl bigint CHECK (bbl >= 1000000000 and bbl < 6000000000),
+    year smallint CHECK (year > 0),
+    quarter smallint CHECK (quarter >= 1 and quarter <= 4),
+    taxclass varchar(2) null,
+    unitcount smallint null,
+    UNIQUE (bbl, year, quarter)
+);
+
+commit;
+
