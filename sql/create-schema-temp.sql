d3c75733a7978719fe5b6cc3e3cd6c8f09060b12
wstlabs <notyet@notyet.org>
2017-04-08 18:36:18 -0400

    temp.taxbills - column changes

--- sql/create-schema-temp.sql
+++ sql/create-schema-temp.sql
@@ -11,8 +11,11 @@ create table temp.taxbills (
     bbl bigint CHECK (bbl >= 1000000000 and bbl < 6000000000),
     year smallint CHECK (year > 0),
     quarter smallint CHECK (quarter >= 1 and quarter <= 4),
+    htype smallint CHECK (htype >= 1 and htype <= 2),
     taxclass varchar(2) null,
     unitcount smallint null,
+    estimated bigint null,
+    amount float(2),
     UNIQUE (bbl, year, quarter)
 );
 
