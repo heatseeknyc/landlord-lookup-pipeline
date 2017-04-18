6291784fa4c548aa5175c5b53eb66dff484664d7
wstlabs <notyet@notyet.org>
2017-04-18 11:29:03 -0400

    move pluto views to top

--- sql/create-views-core.sql
+++ sql/create-views-core.sql
@@ -4,44 +4,6 @@ begin;
 --
 -- Renames columns + introduces BBL column. 
 --
-create view core.registrations as 
-select 
-    registrationid       as id, 
-    public.make_bbl(boroid,block,lot) as bbl,
-    buildingid           as building_id,
-    boroid               as boro_id,
-    housenumber          as house_number,
-    lowhousenumber       as house_number_low,
-    highhousenumber      as house_number_high,
-    streetname           as street_name, 
-    streetcode           as street_code, 
-    zip,
-    block,             
-    lot,
-    bin,
-    communityboard       as cb_id,
-    lastregistrationdate as last_date,
-    registrationenddate  as end_date
-from flat.registrations;
-
-create view core.contacts as 
-select
-    registrationcontactid as id, 
-    registrationid        as registration_id,
-    contacttype           as contact_type,
-    contactdescription    as contact_description,
-    title                 as contact_title,
-    firstname             as contact_first_name, 
-    middleinitial         as contact_middle_initial, 
-    lastname              as contact_last_name,
-    corporationname       as corporation_name, 
-    businesshousenumber   as business_house_number, 
-    businessstreetname    as business_street_name, 
-    businessapartment     as business_apartment,
-    businesscity          as business_city, 
-    businessstate         as business_state,
-    businesszip           as business_zip
-from flat.contacts;
 
 create view core.pluto as 
 select
@@ -97,6 +59,45 @@ create view core.plutox as
 select a.*,coalesce(b.bldg_count,0)
 from core.pluto as a 
 left join core.building_counts as b on b.bbl = a.bbl;
+
+create view core.registrations as 
+select 
+    registrationid       as id, 
+    public.make_bbl(boroid,block,lot) as bbl,
+    buildingid           as building_id,
+    boroid               as boro_id,
+    housenumber          as house_number,
+    lowhousenumber       as house_number_low,
+    highhousenumber      as house_number_high,
+    streetname           as street_name, 
+    streetcode           as street_code, 
+    zip,
+    block,             
+    lot,
+    bin,
+    communityboard       as cb_id,
+    lastregistrationdate as last_date,
+    registrationenddate  as end_date
+from flat.registrations;
+
+create view core.contacts as 
+select
+    registrationcontactid as id, 
+    registrationid        as registration_id,
+    contacttype           as contact_type,
+    contactdescription    as contact_description,
+    title                 as contact_title,
+    firstname             as contact_first_name, 
+    middleinitial         as contact_middle_initial, 
+    lastname              as contact_last_name,
+    corporationname       as corporation_name, 
+    businesshousenumber   as business_house_number, 
+    businessstreetname    as business_street_name, 
+    businessapartment     as business_apartment,
+    businesscity          as business_city, 
+    businessstate         as business_state,
+    businesszip           as business_zip
+from flat.contacts;
 
 -- Omit corrupted BBL/BIN pairs, and add an "active" column
 create view core.dhcr as 
