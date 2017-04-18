685af61bd82dc733c98118b917c08cbfaec5b4cd
wstlabs <notyet@notyet.org>
2017-04-18 11:26:44 -0400

    move pluto tables up to top

--- sql/create-schema-flat.sql
+++ sql/create-schema-flat.sql
@@ -10,58 +10,6 @@ begin;
 drop schema if exists flat cascade;
 create schema flat; 
 
-create table flat.registrations (
-    registrationid integer,
-    buildingid integer,
-    boroid smallint,
-    boro text,
-    housenumber text,
-    lowhousenumber text,
-    highhousenumber text,
-    streetname text,
-    streetcode text,
-    zip text,
-    block smallint,
-    lot smallint, 
-    bin integer,
-    communityboard smallint,
-    lastregistrationdate date,
-    registrationenddate date
-);
-
--- Note that  the 'Type' column in the contacts file, has been 
--- renamed to 'ContactType' to avoid collision on the reserved 
--- SQL word. 
-create table flat.contacts (
-    registrationcontactid integer,
-    registrationid integer,
-    contacttype text,
-    contactdescription text,
-    corporationname text,
-    title text,
-    firstname text,
-    middleinitial text,
-    lastname text,
-    businesshousenumber text,
-    businessstreetname text,
-    businessapartment text,
-    businesscity text,
-    businessstate text,
-    businesszip text
-);
-
-create table flat.taxbills (
-    bbl bigint,
-    active_date date,
-    owner_name text,
-    mailing_address text
-);
-
-create table flat.dhcr_pairs (
-    bbl bigint not null,
-    bin integer not null 
-);
-
 -- MAPPluto version 16v2
 -- Note that BBL, and the 5 lower-case fields at the bottom are generated 
 -- by our ETL process; all other fields are as-is (and appear in the order
@@ -113,6 +61,58 @@ create table flat.buildings (
     radius float,
     parts text,
     points text
+);
+
+create table flat.registrations (
+    registrationid integer,
+    buildingid integer,
+    boroid smallint,
+    boro text,
+    housenumber text,
+    lowhousenumber text,
+    highhousenumber text,
+    streetname text,
+    streetcode text,
+    zip text,
+    block smallint,
+    lot smallint, 
+    bin integer,
+    communityboard smallint,
+    lastregistrationdate date,
+    registrationenddate date
+);
+
+-- Note that  the 'Type' column in the contacts file, has been 
+-- renamed to 'ContactType' to avoid collision on the reserved 
+-- SQL word. 
+create table flat.contacts (
+    registrationcontactid integer,
+    registrationid integer,
+    contacttype text,
+    contactdescription text,
+    corporationname text,
+    title text,
+    firstname text,
+    middleinitial text,
+    lastname text,
+    businesshousenumber text,
+    businessstreetname text,
+    businessapartment text,
+    businesscity text,
+    businessstate text,
+    businesszip text
+);
+
+create table flat.taxbills (
+    bbl bigint,
+    active_date date,
+    owner_name text,
+    mailing_address text
+);
+
+create table flat.dhcr_pairs (
+    bbl bigint not null,
+    bin integer not null 
 );
 
 -- Lien sales data are currently an optional part of the pipeline, and not fully supported
