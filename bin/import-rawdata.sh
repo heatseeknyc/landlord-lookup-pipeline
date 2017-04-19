45c9d370e9552e619f63bd8d134d9a1888278f88
wstlabs <notyet@notyet.org>
2017-04-19 11:34:20 -0400

    move taxbills import statement

--- bin/import-rawdata.sh
+++ bin/import-rawdata.sh
@@ -6,11 +6,11 @@ PIPEARGS='(DELIMITER \"|\", FORMAT CSV, HEADER TRUE)'
 COMMAARGS='(DELIMITER \",\", FORMAT CSV, HEADER TRUE)'
 
 echo '[import] inserting data ...'
-python bin/dopg.py -c '"\copy flat.taxbills FROM '$indir/taxbills.csv' '$COMMAARGS';"'
 python bin/dopg.py -c '"\copy flat.registrations FROM '$indir/registrations.txt' '$PIPEARGS';"'
 python bin/dopg.py -c '"\copy flat.contacts FROM '$indir/contacts-clean.csv' '$PIPEARGS';"'
 python bin/dopg.py -c '"\copy flat.dhcr_pairs FROM '$indir/dhcr_pairs.csv' '$COMMAARGS';"'
 python bin/dopg.py -c '"\copy flat.pluto FROM '$indir/mappluto.csv' '$COMMAARGS';"'
 python bin/dopg.py -c '"\copy flat.buildings FROM '$indir/buildings.csv' '$COMMAARGS';"'
+python bin/dopg.py -c '"\copy flat.taxbills FROM '$indir/taxbills.csv' '$COMMAARGS';"'
 echo '[import] done.'
 
