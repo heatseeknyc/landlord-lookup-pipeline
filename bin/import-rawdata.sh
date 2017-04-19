de23ad4f692195b11135a334d7bbdec1dc360472
wstlabs <notyet@notyet.org>
2017-04-19 11:35:15 -0400

    import taxbills in newer format

--- bin/import-rawdata.sh
+++ bin/import-rawdata.sh
@@ -11,6 +11,6 @@ python bin/dopg.py -c '"\copy flat.contacts FROM '$indir/contacts-clean.csv' '$P
 python bin/dopg.py -c '"\copy flat.dhcr_pairs FROM '$indir/dhcr_pairs.csv' '$COMMAARGS';"'
 python bin/dopg.py -c '"\copy flat.pluto FROM '$indir/mappluto.csv' '$COMMAARGS';"'
 python bin/dopg.py -c '"\copy flat.buildings FROM '$indir/buildings.csv' '$COMMAARGS';"'
-python bin/dopg.py -c '"\copy flat.taxbills FROM '$indir/taxbills.csv' '$COMMAARGS';"'
+python bin/dopg.py -c '"\copy flat.taxbills FROM '$indir/values-2016Q4.csv' '$COMMAARGS';"'
 echo '[import] done.'
 
