7f799c317c3dcf2368f9945c3b21eb7a720f25c5
wstlabs <notyet@notyet.org>
2017-05-07 13:12:26 -0400

    refactor nychpd import

--- bin/import-nychpd-registration.sh
+++ bin/import-nychpd-registration.sh
@@ -5,6 +5,6 @@ indir="$stage/xtracted/nychpd"
 PIPEARGS='(DELIMITER \"|\", FORMAT CSV, HEADER TRUE)'
 
 echo '[import] inserting data ...'
-python bin/dopg.py -c '"\copy flat.nychpd_contact FROM '$indir/contacts-clean.csv' '$PIPEARGS';"' 
+python bin/dopg.py -c '"\copy flat.nychpd_registration FROM '$indir/registrations.txt' '$PIPEARGS';"' 
 echo '[import] done.'
 
