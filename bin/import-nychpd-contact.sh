cb1df7cb9dba9f2c0a5673f66bf75c6b7d2fc00c
wstlabs <notyet@notyet.org>
2017-05-07 14:33:48 -0400

    fix names on nychpd import scripts

--- bin/import-nychpd-contact.sh
+++ bin/import-nychpd-contact.sh
@@ -5,6 +5,6 @@ indir="$stage/xtracted/nychpd"
 PIPEARGS='(DELIMITER \"|\", FORMAT CSV, HEADER TRUE)'
 
 echo '[import] inserting data ...'
-python bin/dopg.py -c '"\copy flat.nychpd_contact FROM '$indir/contacts-clean.csv' '$PIPEARGS';"' 
+python bin/dopg.py -c '"\copy flat.nychpd_contact FROM '$indir/contact-clean.csv' '$PIPEARGS';"' 
 echo '[import] done.'
 
--- bin/import-nychpd-registration.sh
+++ bin/import-nychpd-registration.sh
@@ -5,6 +5,6 @@ indir="$stage/xtracted/nychpd"
 PIPEARGS='(DELIMITER \"|\", FORMAT CSV, HEADER TRUE)'
 
 echo '[import] inserting data ...'
-python bin/dopg.py -c '"\copy flat.nychpd_registration FROM '$indir/registrations.txt' '$PIPEARGS';"' 
+python bin/dopg.py -c '"\copy flat.nychpd_registration FROM '$indir/registration.txt' '$PIPEARGS';"' 
 echo '[import] done.'
 
