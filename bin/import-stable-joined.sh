27bff48503bc7e17022675768e64c28264d2886b
wstlabs <notyet@notyet.org>
2017-05-15 11:55:11 -0400

    rename stable.joined file

--- bin/import-stable-joined.sh
+++ bin/import-stable-joined.sh
@@ -5,6 +5,6 @@ indir="$stage/xtracted/stable"
 COMMAARGS='(DELIMITER \",\", FORMAT CSV, HEADER TRUE)'
 
 echo '[import] inserting data ...'
-python bin/dopg.py -c '"\copy flat.stable_joined FROM '$indir/joined-mini.csv' '$COMMAARGS';"'
+python bin/dopg.py -c '"\copy flat.stable_joined FROM '$indir/joined.csv' '$COMMAARGS';"'
 echo '[import] done.'
 
