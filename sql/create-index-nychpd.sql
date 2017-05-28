4746a1e20338062dc439dee5fb09818139e6a817
wstlabs <notyet@notyet.org>
2017-05-28 13:21:03 -0400

    single space between all index groups

--- sql/create-index-nychpd.sql
+++ sql/create-index-nychpd.sql
@@ -1,16 +1,20 @@
 begin;
+
 create index on push.nychpd_building(id);
 create index on push.nychpd_building(bbl);
 create index on push.nychpd_building(bin);
+
 create index on push.nychpd_registration(id);
 create index on push.nychpd_registration(bbl);
 create index on push.nychpd_registration(building_id);
 -- create index on push.nychpd_registration(house_number,street_name,zip);
 -- create index on push.nychpd_registration(house_number,street_name,boro_id);
+
 create index on push.nychpd_contact(id);
 create index on push.nychpd_contact(contact_type);
 create index on push.nychpd_contact(registration_id);
 create index on push.nychpd_contact_rank(contact_type);
+
 create index on push.nychpd_legal(id);
 create index on push.nychpd_legal(bbl);
 create index on push.nychpd_legal(building_id);
