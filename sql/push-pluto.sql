8b308686aec8386c0b7b8899948514f507955458
wstlabs <notyet@notyet.org>
2017-06-01 18:25:10 -0400

    fix table name

--- sql/push-pluto.sql
+++ sql/push-pluto.sql
@@ -6,8 +6,8 @@ select * from core.pluto_taxlot_remix;
 create table push.pluto_building as
 select * from core.pluto_building;
 
-create table push.pluto_building_primary as
-select * from core.pluto_building_primary;
+create table push.pluto_building_canonical as
+select * from core.pluto_building_canonical;
 
 -- Yes, these next two statements just copy existing tables in-place.
 -- But the tables are small, and it's convenient to have everything 
