ef868907dfc9c35467fb4f7788e23f212ab0415e
wstlabs <notyet@notyet.org>
2017-06-01 18:22:44 -0400

    fix broken select

--- sql/push-misc.sql
+++ sql/push-misc.sql
@@ -1,11 +1,17 @@
 
 begin;
 
-create table push.stable as
-select * from core.stable;
+create table push.misc_stable as
+select * from core.misc_stable;
 
-create table push.stable_joined as
-select * from flat.stable_joined;
+create table push.misc_joined as
+select * from flat.misc_joined;
+
+create table push.misc_liensale as
+select * from core.misc_liensale;
+
+create table push.misc_nycha as
+select * from core.misc_nycha;
 
 commit;
 
