8e047254e31cba0a9250497885a4ab1e021d7f06
wstlabs <notyet@notyet.org>
2017-06-04 19:55:13 -0400

    create order

--- sql/create-acris-index.sql
+++ sql/create-acris-index.sql
@@ -4,11 +4,13 @@ begin;
 create index on push.acris_master(docid);
 create index on push.acris_master(docid,doctag);
 create index on push.acris_master(docid,doctype);
--- create index on push.acris_master(crfn);
--- create index on push.acris_master(docid,crfn);
 
-create index on push.acris_legal(docid);
+create index on push.acris_deeds(docid);
+create index on push.acris_deeds(docid,doctag);
+create index on push.acris_deeds(docid,doctype);
+
 create index on push.acris_legal(bbl);
+create index on push.acris_legal(docid);
 create index on push.acris_legal(docid,bbl);
 
 create index on push.acris_party(docid);
