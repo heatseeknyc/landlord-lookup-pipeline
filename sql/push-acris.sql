a9c600c20ec0ba9ea96f836e3ef8ab875354b2e6
wstlabs <notyet@notyet.org>
2017-06-04 19:58:44 -0400

    fix push table names

--- sql/push-acris.sql
+++ sql/push-acris.sql
@@ -5,11 +5,15 @@
 begin;
 
 drop table if exists push.acris_master cascade;
+drop table if exists push.acris_deeds cascade;
 drop table if exists push.acris_legal cascade;
 drop table if exists push.acris_party cascade;
 
 create table push.acris_master as
-select * from core.master_tidy where doctag = 'DEED';
+select * from core.acris_master_tidy;
+
+create table push.acris_deeds as
+select * from core.acris_master_tidy where doctag = 'DEED';
 
 create table push.acris_legal as
 select * from core.acris_legal;
