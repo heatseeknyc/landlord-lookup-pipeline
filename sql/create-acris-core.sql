defd468586212daa4eb23fee0aea4ee92b9bb64f
wstlabs <notyet@notyet.org>
2017-06-04 19:03:21 -0400

    dedup views - acris_master

--- sql/create-acris-core.sql
+++ sql/create-acris-core.sql
@@ -13,6 +13,9 @@ drop view if exists core.acris_control cascade;
 drop view if exists core.acris_master cascade; 
 drop view if exists core.acris_legal cascade; 
 drop view if exists core.acris_party cascade; 
+drop view if exists core.acris_master_docid_tidy cascade; 
+drop materialized view if exists core.acris_master_docid_count cascade; 
+drop view if exists core.acris_master_clean cascade; 
 
 create view core.acris_control as 
 select
@@ -46,8 +49,20 @@ select
   postal, date_valid_thru 
 from flat.acris_party;
 
-create view core.master_tidy as 
+create view core.acris_master_tidy as 
 select docid,doctag,doctype,amount,percentage,date_filed,date_modified from core.acris_master;
+
+-- This MV is costly time-wise (90 sec or so) but tiny space-wise (currently 749 rows)
+-- and should make the de-duping select in the next view go much faster.
+create materialized view core.acris_master_docid_count as 
+select docid,count(*) as kount from core.acris_master group by docid having count(*) > 1;
+create index on core.acris_master_docid_count(docid);
+
+create view core.acris_master_clean as
+select a.* 
+from      core.acris_master             as a
+left join core.acris_master_docid_count as b on a.docid = b.docid
+where b.docid is null;
 
 commit;
 
