b84ad58ae870ec29aa935378168ea5bcd5377906
wstlabs <notyet@notyet.org>
2017-06-05 12:43:27 -0400

    move most important table to top

--- sql/create-index-hard.sql
+++ sql/create-index-hard.sql
@@ -4,9 +4,9 @@
 
 begin;
 
+create index on hard.property_summary(bbl,bin);
 create index on hard.contact_info(contact_id);
 create index on hard.contact_info(bbl,bin);
-create index on hard.property_summary(bbl,bin);
 create index on hard.pluto_building(bbl);
 
 commit;
