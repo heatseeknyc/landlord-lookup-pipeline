378728ff91114db14a638144db3ea25d088bda72
wstlabs <notyet@notyet.org>
2017-06-05 12:44:14 -0400

    add indices for bbl

--- sql/create-index-hard.sql
+++ sql/create-index-hard.sql
@@ -4,8 +4,10 @@
 
 begin;
 
+create index on hard.property_summary(bbl);
 create index on hard.property_summary(bbl,bin);
 create index on hard.contact_info(contact_id);
+create index on hard.contact_info(bbl);
 create index on hard.contact_info(bbl,bin);
 create index on hard.pluto_building(bbl);
 
