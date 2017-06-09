10398b34dd67464e7defc065442f9d231dd6bc46
wstlabs <notyet@notyet.org>
2017-06-09 11:00:13 -0400

    refine comments / whitespace

--- sql/create-dob-flat.sql
+++ sql/create-dob-flat.sql
@@ -29,17 +29,17 @@ create table flat.dob_permit (
     job_type char(2),
     self_cert char(1), -- appears as either 'Y' or NULL
     block integer,
-    lot text, -- usually 0-padded, 5-digit string but sometimes appears as a garbled mixture of digits and spaces 
-    cb text, -- sometimes appears as a short string of spaces, or as NULL 
+    lot text, -- usually 0-padded, 5-digit string but sometimes appears as a garbled mixture of digits and spaces
+    cb text, -- sometimes appears as a short string of spaces, or as NULL
     zipcode text, -- usually integer or null, but sometimes 5 blank spaces
-    bldg_type text, -- usually 1 or 2, sometimes spaces or null
+    bldg_type text, -- usually 1 or 2, sometimes spaces or NULL
     residential text, -- 'YES' or NULL
     sd1 text,
     sd2 text,
     work_type char(2), -- sometimes NULL or two spaces
-    permit_status text, 
+    permit_status text,
     filing_status char(7) not null, -- either 'INITIAL' or 'RENEWAL'
-    permit_type text, 
+    permit_type text,
     permit_sequence_id integer,
     permit_subtype char(2), -- sometimes NULL or two spaces
     oil_gas char(3), -- usually 'OIL' or 'GAS', but someimtes NULL or three spaces
@@ -55,15 +55,15 @@ create table flat.dob_permit (
     permittee_license_type text,
     permittee_license_id text, -- usually a 7-digit padded integer but sometimes shorter and/or needs trimming
     act_as_superintendent text, -- somtimes 'Y' but only in a few case; otherwise spaces or NULL 
-    permittee_other_title text, 
-    hic_license text, -- usually integer, sometimes needs trimming
+    permittee_other_title text,
+    hic_license text, -- usually 0-padded 7-digit integer, sometimes spaces or NULL
     safety_manager_first_name text,
     safety_manager_last_name text,
     safety_manager_business_name text,
     superintendent_first_and_last_name text,
     superintendent_business_name text,
     owner_business_type text,
-    nonprofit text, -- 'Y' or NULL,
+    nonprofit text, -- 'Y' or NULL
     owner_business_name text,
     owner_first_name text,
     owner_last_name text,
