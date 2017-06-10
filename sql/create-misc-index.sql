f5c429305624a050c52bffec0dba4c482ea2182d
wstlabs <notyet@notyet.org>
2017-06-10 11:35:32 -0400

    move index create

--- sql/create-misc-index.sql
+++ sql/create-misc-index.sql
@@ -4,10 +4,10 @@
 
 begin;
 
-create index on push.misc_stable_confirmed(bbl);
 create index on push.misc_joined(bbl);
 create index on push.misc_joined(bbl,year);
 create index on push.misc_joined(bbl,in_dhcr);
 create index on push.misc_joined(bbl,estimate);
+create index on push.misc_stable_confirmed(bbl);
 
 commit;
