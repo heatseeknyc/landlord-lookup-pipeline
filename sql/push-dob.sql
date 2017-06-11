f346e0f313a50fad212d3b4082d7d9055cb9be7a
wstlabs <notyet@notyet.org>
2017-06-11 19:58:31 -0400

    creative whitespace

--- sql/push-dob.sql
+++ sql/push-dob.sql
@@ -2,7 +2,8 @@
 begin;
 
 create table push.dob_permit as
-select * from core.dob_permit where public.is_valid_bbl(bbl) and public.is_valid_bin(bin);
+select * from core.dob_permit where 
+    public.is_valid_bbl(bbl) and public.is_valid_bin(bin);
 create index on push.dob_permit(bbl);
 create index on push.dob_permit(bin);
 create index on push.dob_permit(bbl,bin);
