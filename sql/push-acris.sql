6adcd143082201a7513200ae316572f326c3c7cf
wstlabs <notyet@notyet.org>
2017-06-11 13:07:17 -0400

    move index creates near table creates - acris

--- sql/push-acris.sql
+++ sql/push-acris.sql
@@ -11,15 +11,26 @@ drop table if exists push.acris_party cascade;
 
 create table push.acris_master as
 select * from core.acris_master_tidy;
+create index on push.acris_master(docid);
+create index on push.acris_master(docid,doctag);
+create index on push.acris_master(docid,doctype);
 
 create table push.acris_deeds as
 select * from core.acris_master_tidy where doctag = 'DEED';
+create index on push.acris_deeds(docid);
+create index on push.acris_deeds(docid,doctag);
+create index on push.acris_deeds(docid,doctype);
 
 create table push.acris_legal as
 select * from core.acris_legal;
+create index on push.acris_legal(bbl);
+create index on push.acris_legal(docid);
+create index on push.acris_legal(docid,bbl);
 
 create table push.acris_party as
 select * from core.acris_party;
+create index on push.acris_party(docid);
+create index on push.acris_party(docid,party_type);
 
 commit;
 
