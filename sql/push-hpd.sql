7676d8e37503b59492b1d2996e3a17dd91780a69
wstlabs <notyet@notyet.org>
2017-06-11 13:58:21 -0400

    add 'drop-if' statements before create table statements

--- sql/push-hpd.sql
+++ sql/push-hpd.sql
@@ -10,6 +10,7 @@ begin;
 -- de-facto primary keys, and which (in principle) can be retrieved by
 -- joining on other tables.
 --
+drop table if exists push.hpd_building cascade;
 create table push.hpd_building as
 select id, bbl, bin, program, dob_class_id, legal_stories, legal_class_a, legal_class_b, lifecycle, status_id
 from core.hpd_building
@@ -21,6 +22,7 @@ drop view if exists push.hpd_building_count cascade;
 create view push.hpd_building_count as 
 select bbl,count(*) as total from push.hpd_building group by bbl;
 
+drop table if exists push.hpd_registration cascade;
 create table push.hpd_registration as
 select id, bbl, building_id, bin, last_date, end_date 
 from core.hpd_registration
@@ -29,6 +31,7 @@ create index on push.hpd_registration(id);
 create index on push.hpd_registration(bbl);
 create index on push.hpd_registration(building_id);
 
+drop table if exists push.hpd_contact cascade;
 create table push.hpd_contact as
 select * from core.hpd_contact;
 create index on push.hpd_contact(id);
@@ -42,6 +45,7 @@ from      push.hpd_registration as a
 left join push.hpd_contact      as b on a.id = b.registration_id
 group by a.bbl;
 
+drop table if exists push.hpd_legal cascade;
 create table push.hpd_legal as
 select id, building_id, bbl, case_type, case_open_date, case_status, case_status_date, case_judgement 
 from core.hpd_legal
@@ -53,6 +57,7 @@ drop view if exists push.hpd_legal_count cascade;
 create view push.hpd_legal_count as 
 select bbl,count(*) as total from push.hpd_legal group by bbl;
 
+drop table if exists push.hpd_complaint cascade;
 create table push.hpd_complaint as
 select * from core.hpd_complaint;
 create index on push.hpd_complaint(id);
@@ -63,6 +68,7 @@ create view push.hpd_complaint_count as
 select bbl,count(*) as total from push.hpd_complaint group by bbl;
 
 -- Not yet sure if we need all these date fields.
+drop table if exists push.hpd_violation cascade;
 create table push.hpd_violation as
 select 
   id, building_id, registration_id, bbl, apt, story, class,
@@ -80,6 +86,7 @@ drop view if exists push.hpd_violation_count cascade;
 create view push.hpd_violation_count as 
 select bbl,count(*) as total from push.hpd_violation group by bbl;
 
+drop table if exists push.hpd_taxlot_summary cascade;
 create table hpd_taxlot_summary as
 select
   coalesce(a.bbl,b.bbl,c.bbl,d.bbl,e.bbl) as bbl,
@@ -108,6 +115,7 @@ create index on hpd_taxlot_summary(bbl);
 -- at the very bottom.
 --
 
+drop table if exists push.hpd_contact_rank cascade;
 create table push.hpd_contact_rank ( 
     id integer,
     contact_type text
@@ -124,7 +132,6 @@ insert into push.hpd_contact_rank (id,contact_type) values
     (9,'Lessee')
 ;
 create index on push.hpd_contact_rank(contact_type);
-
 
 commit;
 
