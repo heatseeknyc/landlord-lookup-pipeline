a91ccd21af7b2226f4e0efde3103c58fe8d4f355
wstlabs <notyet@notyet.org>
2017-06-11 20:25:56 -0400

    push dob.violation, dob.complaint

--- sql/push-dob.sql
+++ sql/push-dob.sql
@@ -8,4 +8,15 @@ create index on push.dob_permit(bbl);
 create index on push.dob_permit(bin);
 create index on push.dob_permit(bbl,bin);
 
+create table push.dob_violation as
+select * from core.dob_violation where 
+    public.is_valid_bbl(bbl) and public.is_valid_bin(bin);
+create index on push.dob_violation(bbl);
+create index on push.dob_violation(bin);
+create index on push.dob_violation(bbl,bin);
+
+create table push.dob_complaint as
+select * from core.dob_complaint where public.is_valid_bin(bin);
+create index on push.dob_complaint(bin);
+
 commit;
