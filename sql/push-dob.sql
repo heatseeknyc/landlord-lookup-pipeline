cadf95f1f4c5fa85ef8f8111699902084e8a18ed
wstlabs <notyet@notyet.org>
2017-06-11 20:40:39 -0400

    counting views for permit, violation

--- sql/push-dob.sql
+++ sql/push-dob.sql
@@ -7,6 +7,9 @@ select * from core.dob_permit where
 create index on push.dob_permit(bbl);
 create index on push.dob_permit(bin);
 create index on push.dob_permit(bbl,bin);
+drop view if exists push.dob_permit_count cascade;
+create view push.dob_permit_count as  
+select bbl,bin,count(*) as total from push.dob_permit group by bbl,bin;
 
 create table push.dob_violation as
 select * from core.dob_violation where 
@@ -14,6 +17,9 @@ select * from core.dob_violation where
 create index on push.dob_violation(bbl);
 create index on push.dob_violation(bin);
 create index on push.dob_violation(bbl,bin);
+drop view if exists push.dob_violation_count cascade;
+create view push.dob_violation_count as  
+select bbl,bin,count(*) as total from push.dob_violation group by bbl,bin;
 
 create table push.dob_complaint as
 select * from core.dob_complaint where public.is_valid_bin(bin);
