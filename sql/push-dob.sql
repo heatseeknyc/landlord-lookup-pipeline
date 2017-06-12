8c465a54bb7ef0e93a9e1aa11df51ed9f87ba003
wstlabs <notyet@notyet.org>
2017-06-11 20:55:18 -0400

    aggregate by bin, not bbl,bin

--- sql/push-dob.sql
+++ sql/push-dob.sql
@@ -9,7 +9,7 @@ create index on push.dob_permit(bin);
 create index on push.dob_permit(bbl,bin);
 drop view if exists push.dob_permit_count cascade;
 create view push.dob_permit_count as  
-select bbl,bin,count(*) as total from push.dob_permit group by bbl,bin;
+select bin,count(*) as total from push.dob_permit group by bin;
 
 create table push.dob_violation as
 select * from core.dob_violation where 
@@ -19,7 +19,7 @@ create index on push.dob_violation(bin);
 create index on push.dob_violation(bbl,bin);
 drop view if exists push.dob_violation_count cascade;
 create view push.dob_violation_count as  
-select bbl,bin,count(*) as total from push.dob_violation group by bbl,bin;
+select bin,count(*) as total from push.dob_violation group by bin;
 
 create table push.dob_complaint as
 select * from core.dob_complaint where public.is_valid_bin(bin);
