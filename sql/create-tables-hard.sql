5de3bfdbe34fa5b72f822e7faaaf69b031e0fdc8
wstlabs <notyet@notyet.org>
2017-06-11 22:43:59 -0400

    move index create statements near table create statements

--- sql/create-tables-hard.sql
+++ sql/create-tables-hard.sql
@@ -8,18 +8,19 @@ begin;
 
 create table hard.property_summary as
 select * from meta.property_summary;
+create index on hard.property_summary(bbl);
+create index on hard.property_summary(bbl,bin);
 
 create table hard.contact_info as
 select * from meta.contact_info;
+create index on hard.contact_info(contact_id);
+create index on hard.contact_info(bbl);
+create index on hard.contact_info(bbl,bin);
 
 create table hard.pluto_building as
 select * from push.pluto_building; 
-
---
--- Deprecated for the time being.
---
--- create table hard.registrations as 
--- select * from push.registrations;
+create index on hard.pluto_building(bbl);
+create index on hard.pluto_building(doitt_id);
 
 commit;
 
