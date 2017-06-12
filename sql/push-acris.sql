17ab23dfa185b2421b165c8eb520f4fa9331cf9e
wstlabs <notyet@notyet.org>
2017-06-12 09:16:27 -0400

    move drop-if statements near create statements

--- sql/push-acris.sql
+++ sql/push-acris.sql
@@ -5,28 +5,27 @@
 begin;
 
 drop table if exists push.acris_master cascade;
-drop table if exists push.acris_deeds cascade;
-drop table if exists push.acris_legal cascade;
-drop table if exists push.acris_party cascade;
-
 create table push.acris_master as
 select * from core.acris_master_tidy;
 create index on push.acris_master(docid);
 create index on push.acris_master(docid,doctag);
 create index on push.acris_master(docid,doctype);
 
+drop table if exists push.acris_deeds cascade;
 create table push.acris_deeds as
 select * from core.acris_master_tidy where doctag = 'DEED';
 create index on push.acris_deeds(docid);
 create index on push.acris_deeds(docid,doctag);
 create index on push.acris_deeds(docid,doctype);
 
+drop table if exists push.acris_legal cascade;
 create table push.acris_legal as
 select * from core.acris_legal;
 create index on push.acris_legal(bbl);
 create index on push.acris_legal(docid);
 create index on push.acris_legal(docid,bbl);
 
+drop table if exists push.acris_party cascade;
 create table push.acris_party as
 select * from core.acris_party;
 create index on push.acris_party(docid);
