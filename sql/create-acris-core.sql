16b621bed3f62bee889e87984b05e5a750c3a29b
wstlabs <notyet@notyet.org>
2017-06-11 23:46:01 -0400

    move drop-if statements near create statements

--- sql/create-acris-core.sql
+++ sql/create-acris-core.sql
@@ -9,14 +9,8 @@
 
 begin;
 
-drop view if exists core.acris_control cascade; 
-drop view if exists core.acris_master cascade; 
-drop view if exists core.acris_legal cascade; 
-drop view if exists core.acris_party cascade; 
-drop view if exists core.acris_master_docid_tidy cascade; 
-drop materialized view if exists core.acris_master_docid_count cascade; 
-drop view if exists core.acris_master_clean cascade; 
 
+drop view if exists core.acris_control cascade; 
 create view core.acris_control as 
 select
   doctype, description, classcode, ptype1, ptype2, ptype3,
@@ -29,6 +23,7 @@ select
   end as doctag
 from flat.acris_control;
 
+drop view if exists core.acris_master cascade; 
 create view core.acris_master as 
 select 
   a.docid, a.crfn, a.boro, b.doctag, a.doctype, a.amount, a.percentage, a.reel_year, a.reel_number, a.reel_page,  
@@ -36,12 +31,14 @@ select
 from flat.acris_master            as a 
 left join core.acris_control as b on a.doctype = b.doctype; 
 
+drop view if exists core.acris_legal cascade; 
 create view core.acris_legal as 
 select 
   docid, public.make_bbl(boro,block,lot) as bbl, easement, partial, rights_air, rights_sub, proptype,
   street_name, street_number, unit, date_valid_thru 
 from flat.acris_legal;
 
+drop view if exists core.acris_party cascade; 
 create view core.acris_party as 
 select 
   docid, party_type, name, address1, address2, country, city, 
@@ -51,10 +48,12 @@ from flat.acris_party;
 
 -- This MV is costly time-wise (90 sec or so) but tiny space-wise (currently 749 rows)
 -- and should make the de-duping select in the next view go much faster.
+drop materialized view if exists core.acris_master_docid_count cascade; 
 create materialized view core.acris_master_docid_count as 
 select docid,count(*) as kount from core.acris_master group by docid having count(*) > 1;
 create index on core.acris_master_docid_count(docid);
 
+drop view if exists core.acris_master_clean cascade; 
 create view core.acris_master_clean as
 select a.* 
 from      core.acris_master             as a
@@ -62,9 +61,9 @@ left join core.acris_master_docid_count as b on a.docid = b.docid
 where b.docid is null;
 
 -- A tidier view of the master table, restricted to essential columns 
+drop view if exists core.acris_master_docid_tidy cascade; 
 create view core.acris_master_tidy as 
 select docid,doctag,doctype,amount,percentage,date_filed,date_modified from core.acris_master_clean;
-
 
 commit;
 
