e7cb6a4dc431c38d3415edbd47e90f768023fe03
wstlabs <notyet@notyet.org>
2017-06-16 12:53:37 -0400

    residential label

--- sql/deco-view.sql
+++ sql/deco-view.sql
@@ -1,12 +1,15 @@
 begin;
 
+--
 -- A final view on outgoing data which is basically equivalent to the table 
 -- of the same name in the 'hard' schema, but with (verbose) reference data 
 -- labels slotted in.  Doing it this way will should save substantial space,
 -- with minimal time cost. 
+--
 create view deco.property_summary as
 select 
     a.*, 
+    a.residential_likely or a.stable_status is not null as residential,
     b.label as pluto_bldg_class_label, c.label as pluto_land_use_label
 from hard.property_summary as a
 left join push.pluto_refdata_bldgclass as b on a.pluto_bldg_class = b.tag
