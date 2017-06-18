f8e5df65c2659b917636b2f88521e3f6fa56477e
wstlabs <notyet@notyet.org>
2017-06-18 15:14:29 -0400

    new function - pivotrecs

--- etlapp/special/match.py
+++ etlapp/special/match.py
@@ -1,9 +1,12 @@
 import os
+import copy
+from collections import OrderedDict
 from etlapp.logging import log
 from etlapp.decorators import timedsingle
 from etlapp.util.source import splitpath, tablename
 from etlapp.util.io import read_recs
 from etlapp.shell import dopsql
+import etlapp.util.nycgeo as nycgeo
 import etlapp.source
 import etlapp.stage
 import etlapp
@@ -72,12 +75,46 @@ def loadstreams(_pluto,_acris):
     count['acris'] = len(acris)
     return pluto,acris,count
 
+def loadtables(_pluto,_acris):
+    log.debug("..")
+    pluto,acris,count = loadstreams(_pluto,_acris)
+    log.debug("count = %s" % count)
+    log.debug("pivot pluto ..")
+    pluto = pivotrecs(pluto,inplace=True)
+    log.debug("pivot acris..")
+    acris = pivotrecs(acris,inplace=True)
+    # In theory, there's no way the dict counts can differ from the stream counts.
+    # But on general principles, we should redoc the counts anyway.
+    count['pluto'] = len(pluto)
+    count['acris'] = len(acris)
+    return pluto,acris,count
+
+
+def pivotrecs(recs,inplace=False):
+    """Pivots a stream of incomings, presumed to have at least one key 'bbl'
+    (which is also presumed to be unique throughout the stream) into a nice OrderedDict
+    struct of {bbl:r}, where :r is a deepcopy of the truncated record dict.
+
+    An optiona flag, :inplace, performs the record-munging operation in-place
+    rather than on cloned dict.  This will of course trash the incoming record stream,
+    but will be significiantly faster (about 3x), round-trip.
+    """
+    d = OrderedDict()
+    for r in recs:
+        bbl = r['bbl']
+        if not nycgeo.is_valid_bbl(bbl):
+            raise ValueError("invalid BBL '%s' detected in input stream" % bbl)
+        if bbl in d:
+            raise ValueError("duplicate BBL '%s' detected in input stream" % bbl)
+        rr = r if inplace else copy.deepcopy(r)
+        del rr['bbl']
+        d[bbl] = rr
+    return d
+
 @timedsingle
 def match_streams(_pluto,_acris):
-    pluto,acris,count = loadstreams(_pluto,_acris)
+    pluto,acris,count = loadtables(_pluto,_acris)
     log.info("count = %s" % count)
-    print(pluto[0])
-    print(acris[0])
     return True
 
 
