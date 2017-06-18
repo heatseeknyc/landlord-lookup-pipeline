8bfca4c0687ddd18f1cd4a82dd255f51557be63f
wstlabs <notyet@notyet.org>
2017-06-18 11:56:06 -0400

    enhanced dump command

--- etlapp/util/dump.py
+++ etlapp/util/dump.py
@@ -1,7 +1,13 @@
 from etlapp.util.csvarg import make_csv_args
 
+
+"""
+We use the '(select % from ...)' construct so that the relation we pull
+from doesn't have to be table, as it would be if given as the direct argument
+to the COPY command.  Introduces a very slight performance overhead, but is
+vastly more flexible.
+"""
 def make_dump_command(table,outfile,char=','):
     csvargs = make_csv_args(char)
-    # COPY products_273 TO '/tmp/products_199.csv' DELIMITER ',' CSV HEADER;^
-    return "\copy %s TO %s %s;" % (table,outfile,csvargs)
+    return "\copy (select * from %s) TO %s %s;" % (table,outfile,csvargs)
 
