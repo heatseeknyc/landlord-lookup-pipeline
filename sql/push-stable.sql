8cfcbaab48cbf349cfd99abffad8279755ea1250
wstlabs <notyet@notyet.org>
2017-06-21 12:54:54 -0400

    add drop-of statement before create

--- sql/push-stable.sql
+++ sql/push-stable.sql
@@ -1,6 +1,7 @@
 
 begin;
 
+drop table if exists push.stable_combined cascade; 
 create table push.stable_combined as
 select * from core.stable_combined;
 create index on push.stable_combined(bbl);
