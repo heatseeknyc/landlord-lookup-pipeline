89588213aaf1d86a0d6015e42f8518a1f142f850
wstlabs <notyet@notyet.org>
2017-06-25 15:00:36 -0400

    line breaks before new section

--- sql/push-pluto.sql
+++ sql/push-pluto.sql
@@ -5,18 +5,14 @@ select * from core.pluto_taxlot_remix;
 create index on push.pluto_taxlot(bbl);
 
 create view push.pluto_taxlot_tidy as
-select bbl, address, owner_name, bldg_class, land_use, year_built, units_total, units_res, num_bldgs, bldg_count
+select bbl, address, owner_name, bldg_class, land_use, year_built, units_total, units_res, num_floors, num_bldgs, bldg_count
 from push.pluto_taxlot;
 
 create table push.pluto_building as
-select * from core.pluto_building;
-create index on push.pluto_building(bbl,bin);
-
-create table push.pluto_building_ideal as
 select * from core.pluto_building_ideal;
-create index on push.pluto_building_ideal(bbl);
-create index on push.pluto_building_ideal(bin);
-create index on push.pluto_building_ideal(bbl,bin);
+create index on push.pluto_building(bbl);
+create index on push.pluto_building(bin);
+create index on push.pluto_building(bbl,bin);
 
 create materialized view push.pluto_building_kosher as
 select * from push.pluto_building_ideal where is_kosher_bin(bin);
@@ -26,10 +22,11 @@ create index on push.pluto_building_kosher(bbl,bin);
 
 
 
-
+/*
 create table push.pluto_building_canonical as
 select * from core.pluto_building_canonical;
 create index on push.pluto_building_canonical(bbl,bin);
+*/
 
 -- Yes, these next two statements just copy existing tables from the 'flat' schema. 
 -- But the tables are small, and it's convenient to have everything together here 
@@ -46,6 +43,13 @@ create index on push.pluto_refdata_landuse(tag);
 --
 -- Analytical views
 --
+
+
+
+
+
+
+
 
 -- A pre-baked table of primary condo lots, with qualified block numbes slotted in.
 -- Yields 7440 rows in 16v2.
