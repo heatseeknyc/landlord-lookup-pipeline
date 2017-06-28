8ab3d75009712d41180b5cc57f402e7d60c4547f
wstlabs <notyet@notyet.org>
2017-06-28 12:23:29 -0400

    docstrings and such

--- tests/etlapp/stage.py
+++ tests/etlapp/stage.py
@@ -10,18 +10,30 @@ from collections import OrderedDict
 import etlapp.stage as stage
 import etlapp.source as source
 
+"""
+The names for these methods are kind of loosey-goosey.
+But hey, it's just a test suite.
+"""
+
 def datafor(prefix,name):
+    """Provides the "data for" the given pair of (:prefix,:name)."""
     active = source.getval(prefix,name,'active')
     path = stage.latest(prefix,name)
     return active,path
 
 def resolve(prefix):
+    """
+    Resolves a prefix into an OrderedDict of (name,values), where :values is
+    a tuple of values of interest.
+    """
     names = source.names(prefix)
     print("names[%s] = %s" % (prefix,names))
     pairs = ((name,datafor(prefix,name)) for name in names)
     return OrderedDict(pairs)
 
 def construct(sources):
+    """Construct our big nested struct from a sequence of prefixes.
+    Reutrns: a nifty OrderdDict-of-OrderedDict-of-sequence struct."""
     pairs = ((prefix,resolve(prefix)) for prefix in sources)
     return OrderedDict(pairs)
 
