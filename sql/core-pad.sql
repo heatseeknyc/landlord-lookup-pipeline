c83d72c6ad25217659f670c2bb999b46280a4724
wstlabs <notyet@notyet.org>
2017-06-29 12:51:16 -0400

    new view - core.pad_adr

--- sql/core-pad.sql
+++ sql/core-pad.sql
@@ -1,6 +1,36 @@
 
 begin;
 
+-- All columns cast in the obvious way. 
+drop view if exists core.pad_adr cascade; 
+create view core.pad_adr as
+select
+    soft_bbl(boro,block,lot) as bbl,
+    soft_int(bin) as bin,
+    normfw(lhnd) as lhnd, 
+    normfw(lhns) as lhns, 
+    normfw(lcontpar) as lcontpar, 
+    normfw(lsos) as lsos,
+    normfw(hhnd) as hhnd, 
+    normfw(hhns) as hhns, 
+    normfw(hcontpar) as hcontpar, 
+    normfw(hsos) as hsos,
+    soft_int(scboro)::smallint as scboro, 
+    normfw(sc5) as sc5,
+    normfw(sclgc) as sclgc,
+    normfw(stname) as stname,
+    normfw(addrtype) as addrtype, 
+    normfw(realb7sc) as realb7sc, 
+    normfw(validlgcs) as validlgcs, 
+    normfw(dapsflag) as dapsflag,
+    normfw(naubflag) as naubflag, 
+    soft_int(parity)::smallint as parity,
+    normfw(b10sc) as b10sc,
+    soft_int(segid) as segid,
+    soft_int(zipcode) as zipcode, 
+    soft_int(physical_id) as physical_id 
+from flat.pad_adr;
+
 -- Creates BBLs + casts other columns as appropriate. 
 -- Drops the 3 SCC columns, as these don't seem to have any external meaning. 
 drop view if exists core.pad_bbl cascade; 
