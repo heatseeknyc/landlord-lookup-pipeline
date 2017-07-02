7e1a02c60d6ef2d3d4206e6531ec219a3809fe23
wstlabs <notyet@notyet.org>
2017-07-02 13:21:44 -0400

    count condos

--- task/count-flavors.sql
+++ task/count-flavors.sql
@@ -10,24 +10,28 @@ select x.prefix, x.source, x.flavor, x.uniqbbl, x.rowcount from (
   select 3, 'pluto', 'building', 'regular', count(*), sum(total) from core.pluto_building_count where is_regular_bbl(bbl) union
   select 4, 'pluto', 'building', 'marginal', count(*), sum(total) from core.pluto_building_count where is_marginal_bbl(bbl) union
   select 5, 'pluto', 'building', 'degenerate', count(*), sum(total)  from core.pluto_building_count where is_degenerate_bbl(bbl) union 
+  select 6, 'pluto', 'building', 'condo', count(*), sum(total)  from core.pluto_building_count where is_condo_bbl(bbl) union 
 
   select 1, 'pluto', 'taxlot', 'total', count(*), NULL from core.pluto_taxlot union 
   select 2, 'pluto', 'taxlot', 'valid', count(*), NULL from core.pluto_taxlot where is_valid_bbl(bbl) union 
   select 3, 'pluto', 'taxlot', 'regular', count(*), NULL  from core.pluto_taxlot where is_regular_bbl(bbl) union
   select 4, 'pluto', 'taxlot', 'marginal', count(*), NULL  from core.pluto_taxlot where is_marginal_bbl(bbl) union 
   select 5, 'pluto', 'taxlot', 'degenerate', count(*), NULL from core.pluto_taxlot where is_degenerate_bbl(bbl) union
+  select 6, 'pluto', 'taxlot', 'condo', count(*), NULL from core.pluto_taxlot where is_condo_bbl(bbl) union
 
   select 1, 'acris', 'legal', 'total' as flavor, count(*), sum(total) from push.acris_legal_count union 
   select 2, 'acris', 'legal', 'valid', count(*), sum(total) from push.acris_legal_count where is_valid_bbl(bbl) union 
   select 3, 'acris', 'legal', 'regular', count(*), sum(total) from push.acris_legal_count where is_regular_bbl(bbl) union
   select 4, 'acris', 'legal', 'marginal', count(*), sum(total) from push.acris_legal_count where is_marginal_bbl(bbl) union 
   select 5, 'acris', 'legal', 'degenerate', count(*), sum(total) from push.acris_legal_count where is_degenerate_bbl(bbl) union
+  select 6, 'acris', 'legal', 'condo', count(*), sum(total) from push.acris_legal_count where is_condo_bbl(bbl) union
 
-  select 1, 'pad', 'bbl', 'total' as flavor, count(*), NULL from push.dcp_pad_adr_count union
-  select 2, 'pad', 'bbl', 'valid', count(*), NULL from push.dcp_pad_adr_count where is_valid_bbl(bbl) union
-  select 3, 'pad', 'bbl', 'regular', count(*), NULL from push.dcp_pad_adr_count where is_regular_bbl(bbl) union
-  select 4, 'pad', 'bbl', 'marginal', count(*), NULL from push.dcp_pad_adr_count where is_marginal_bbl(bbl) union
-  select 5, 'pad', 'bbl', 'degenerate', count(*), NULL from push.dcp_pad_adr_count where is_degenerate_bbl(bbl)
+  select 1, 'dcp', 'pad-adr', 'total' as flavor, count(*), NULL from push.dcp_pad_adr_count union
+  select 2, 'dcp', 'pad-adr', 'valid', count(*), NULL from push.dcp_pad_adr_count where is_valid_bbl(bbl) union
+  select 3, 'dcp', 'pad-adr', 'regular', count(*), NULL from push.dcp_pad_adr_count where is_regular_bbl(bbl) union
+  select 4, 'dcp', 'pad-adr', 'marginal', count(*), NULL from push.dcp_pad_adr_count where is_marginal_bbl(bbl) union
+  select 5, 'dcp', 'pad-adr', 'degenerate', count(*), NULL from push.dcp_pad_adr_count where is_degenerate_bbl(bbl) union
+  select 6, 'dcp', 'pad-adr', 'condo', count(*), NULL from push.dcp_pad_adr_count where is_condo_bbl(bbl)
 
 ) as x order by prefix, source, rank;
 
