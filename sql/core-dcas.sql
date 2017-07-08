2b5c7538bb86f42c483f0b1db380ba203de5d6b1
wstlabs <notyet@notyet.org>
2017-07-08 11:21:14 -0400

    refactor dcas views

--- sql/core-dcas.sql
+++ sql/core-dcas.sql
@@ -1,8 +1,8 @@
 
 begin;
 
-drop table if exists core.dcas_law48 cascade; 
-create table core.dcas_law48 as
+drop view if exists core.dcas_law48 cascade; 
+create view core.dcas_law48 as
 select
     make_bbl(boro,block,lot) as bbl,
     coordinates,
@@ -26,11 +26,17 @@ select
     potential_urban,
     current_uses 
 from flat.dcas_law48;
-create index on core.dcas_law48(bbl);
 
 create view core.dcas_law48_count as
 select bbl, count(*), max(created) as latest 
 from core.dcas_law48 group by bbl; 
+
+create view core.dcas_law48_tidy as
+select
+    a.bbl, a.latest,
+    b.coordinates, b.address, b.parcel_name, b.agency, b.easements 
+from core.dcas_law48_count as a
+left join core.dcas_law48  as b on (a.bbl,a.latest) = (b.bbl,b.created);
 
 commit;
 
