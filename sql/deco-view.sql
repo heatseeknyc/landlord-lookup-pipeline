8988773d3e91f7d715983d1289db26cfb238bfd6
wstlabs <notyet@notyet.org>
2017-07-09 01:30:49 -0400

    drop-if

--- sql/deco-view.sql
+++ sql/deco-view.sql
@@ -16,6 +16,7 @@ from hard.property_summary as a
 left join push.pluto_refdata_bldgclass as b on a.pluto_bldg_class = b.tag
 left join push.pluto_refdata_landuse as c on a.pluto_land_use = c.tag; */
 
+drop view if exists deco.taxlot cascade; 
 create view deco.taxlot as
 select
     a.*,
