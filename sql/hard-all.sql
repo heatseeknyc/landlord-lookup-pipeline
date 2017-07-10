51a985ef4be662b1118d07e23034eca84d6abfb5
wstlabs <notyet@notyet.org>
2017-07-09 21:26:29 -0400

    prune deprecated tables

--- sql/hard-all.sql
+++ sql/hard-all.sql
@@ -6,26 +6,12 @@
 
 begin;
 
-/* 
-drop table if exists hard.property_summary cascade;
-create table hard.property_summary as
-select * from meta.property_summary;
-create index on hard.property_summary(bbl);
-create index on hard.property_summary(bbl,bin);
-*/
-
 drop table if exists hard.contact_info cascade;
 create table hard.contact_info as
 select * from meta.contact_info;
 create index on hard.contact_info(contact_id);
 create index on hard.contact_info(bbl);
 create index on hard.contact_info(bbl,bin);
-
-drop table if exists hard.pluto_building cascade;
-create table hard.pluto_building as
-select * from push.pluto_building; 
-create index on hard.pluto_building(bbl);
-create index on hard.pluto_building(doitt_id);
 
 drop table if exists hard.taxlot cascade;
 create table hard.taxlot as select * from meta.taxlot;
