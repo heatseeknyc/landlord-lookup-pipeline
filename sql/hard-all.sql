18f9f5cd5719185084c1def44f0664a79355acc0
wstlabs <notyet@notyet.org>
2017-07-10 18:13:01 -0400

    copy pluto refdata tables

--- sql/hard-all.sql
+++ sql/hard-all.sql
@@ -23,5 +23,16 @@ create index on hard.building(bbl);
 create index on hard.building(bin);
 create index on hard.building(bbl,bin);
 
+-- These we simply copy over so they're all in the hard schema.
+drop table if exists hard.pluto_refdata_bldgclass cascade;
+create table hard.pluto_refdata_bldgclass as
+select * from push.pluto_refdata_bldgclass;
+create index on hard.pluto_refdata_bldgclass(tag);
+
+drop table if exists hard.pluto_refdata_landuse cascade;
+create table hard.pluto_refdata_landuse as
+select * from push.pluto_refdata_landuse;
+create index on hard.pluto_refdata_landuse(tag);
+
 commit;
 
