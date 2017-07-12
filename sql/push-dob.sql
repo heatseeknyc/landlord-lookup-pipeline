fe64745df2b5e7b964052b89e27865439d1452c6
wstlabs <notyet@notyet.org>
2017-07-12 16:54:32 -0400

    fix typo

--- sql/push-dob.sql
+++ sql/push-dob.sql
@@ -39,7 +39,7 @@ drop view if exists push.dob_permit_tidy cascade;
 create view push.dob_permit_tidy as
 select 
     bbl, bin, 
-    job_id, permit_sequence_idas seq_id, job_doc_id as doc_id, 
+    job_id, permit_sequence_id as seq_id, job_doc_id as doc_id,
     job_type, work_type, permit_status, filing_status, permit_type as permtype, permit_subtype as subtype,
     filing_date, start_date
 from push.dob_permit;
