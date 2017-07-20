4eb82a0a906af42301c61bb0d36fb9f8ee02125d
wstlabs <notyet@notyet.org>
2017-07-20 12:39:15 -0400

    add omni schema

--- sql/create-schema.sql
+++ sql/create-schema.sql
@@ -5,6 +5,7 @@ drop schema if exists flat cascade; create schema flat;
 drop schema if exists core cascade; create schema core; 
 drop schema if exists push cascade; create schema push; 
 drop schema if exists meta cascade; create schema meta; 
+drop schema if exists meta cascade; create schema omni; 
 drop schema if exists hard cascade; create schema hard; 
 drop schema if exists deco cascade; create schema deco; 
 drop schema if exists norm cascade; create schema norm;
