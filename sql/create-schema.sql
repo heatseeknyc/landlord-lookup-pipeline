efbb50ca222f9084b296325453feaf23b3460fcb
wstlabs <notyet@notyet.org>
2017-07-20 12:41:37 -0400

    comments, sections for schema types

--- sql/create-schema.sql
+++ sql/create-schema.sql
@@ -1,13 +1,19 @@
 
 begin;
 
+-- Loading + Procesing schema
 drop schema if exists flat cascade; create schema flat; 
 drop schema if exists core cascade; create schema core; 
 drop schema if exists push cascade; create schema push; 
 drop schema if exists meta cascade; create schema meta; 
 drop schema if exists meta cascade; create schema omni; 
+
+-- Output schema.  These are the only ones that get exported to
+-- the gateway, and the only ones to which the 'readuser' has access.
 drop schema if exists hard cascade; create schema hard; 
 drop schema if exists deco cascade; create schema deco; 
+
+-- Special scheme, used for exports or ad-hoc imports 
 drop schema if exists norm cascade; create schema norm;
 
 commit;
