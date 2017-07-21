70117150d6b9ebaf42f78508e444f59bf95ddf4d
wstlabs <notyet@notyet.org>
2017-07-20 22:24:49 -0400

    schema - misc.dev

--- sql/flat-misc.sql
+++ sql/flat-misc.sql
@@ -22,5 +22,17 @@ create table flat.misc_nycha (
     boroid smallint
 );
 
+create table flat.misc_dev_ent (
+    id integer primary key,
+    name text not null
+);
+
+create table flat.misc_dev_rel (
+    bbl bigint CHECK (bbl >= 1000000000 and bbl < 6000000000),
+    devid integer primary key,
+    remark text,
+    UNIQUE (bbl, devid)
+);
+
 commit;
 
