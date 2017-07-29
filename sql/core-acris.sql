ead4b15a38e06f365d4cca6f217c3ee5e70f6c73
wstlabs <notyet@notyet.org>
2017-07-28 23:08:51 -0400

    revert aliases

--- sql/core-acris.sql
+++ sql/core-acris.sql
@@ -36,7 +36,7 @@ select
   a.docid, a.crfn, a.boro, b.doctag, a.doctype, a.amount, a.percentage, a.reel_year, a.reel_number, a.reel_page,
   case when
       public.is_valid_mmddyyyy(a.date_document) then a.date_document::date else NULL
-  end as date_document,
+  end as date_document, 
   a.date_filed, a.date_modified, a.date_valid_thru
 from flat.acris_master               as a
 left join core.acris_refdata_control as b on a.doctype = b.doctype; 
