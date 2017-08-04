c673f9e8b72dd49ab8ca3b2948062bae9d6c0086
wstlabs <notyet@notyet.org>
2017-08-03 20:09:40 -0400

    add tidy view

--- sql/push-dcas.sql
+++ sql/push-dcas.sql
@@ -9,5 +9,9 @@ drop table if exists push.dcas_ipis cascade;
 create table push.dcas_ipis as select * from core.dcas_ipis;
 create index on push.dcas_ipis(bbl);
 
+create view push.dcas_ipis_tidy as
+select bbl, parcel_name, parcel_address, juris, agency, rpad, water, irreg, owntype, bldg_count renewal
+from push.dcas_ipis;
+
 commit;
 
