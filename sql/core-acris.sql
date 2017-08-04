63e7b68b95601471863673d9d6aac7752ae72c9b
wstlabs <notyet@notyet.org>
2017-08-04 10:21:25 -0400

    refactor master view

--- sql/core-acris.sql
+++ sql/core-acris.sql
@@ -25,21 +25,21 @@ from flat.acris_refdata_control;
 create index on core.acris_refdata_control(doctype);
 
 -- A nearly trivial push, just to have it in the core schema (and indexed).
-drop materialized view if exists core.acris_refdata_docfam cascade;
-create materialized view core.acris_refdata_docfam as
+drop table if exists core.acris_refdata_docfam cascade;
+create table view core.acris_refdata_docfam as
 select * from flat.acris_refdata_docfam;
 create index on core.acris_refdata_docfam(doctype);
 
 drop view if exists core.acris_master cascade;
 create view core.acris_master as
 select
-  a.docid, a.crfn, a.boro, b.doctag, a.doctype, a.amount, a.percentage, a.reel_year, a.reel_number, a.reel_page,
+  docid, crfn, boro, doctype, amount, percentage, reel_year, reel_number, reel_page,
   case when
-      public.is_valid_mmddyyyy(a.date_document) then a.date_document::date else NULL
+      public.is_valid_mmddyyyy(date_document) then date_document::date else NULL
   end as date_document, 
-  a.date_filed, a.date_modified, a.date_valid_thru
-from flat.acris_master               as a
-left join core.acris_refdata_control as b on a.doctype = b.doctype; 
+  date_filed, date_modified, date_valid_thru
+from flat.acris_master;  --             as a;
+-- left join core.acris_refdata_control as b on doctype = b.doctype; 
 
 -- An "information-preserving" mapping of the original flatfile columns as follows: 
 --  - Merges (boro, block, lot) into an integer bbl, 
