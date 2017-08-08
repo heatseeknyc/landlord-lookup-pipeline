6819d6beb7c95caee8d96b4af33b0601e5835d1a
wstlabs <notyet@notyet.org>
2017-08-08 13:29:04 -0400

    ws

---
+++ sql/push-dof.sql
@@ -0,0 +1,26 @@
+
+begin;
+
+create table push.dof_rolling as
+select * from core.dof_rolling;
+create index on push.dof_rolling(bbl);
+
+create view push.dof_rolling_tidy as
+select 
+    bbl, 
+    taxclass_atsale as taxclass, 
+    bldgclass_atsale as bldgclass,
+    address,
+    apartment as apt,
+    easement as ease,
+    sale_price, 
+    sale_date  
+from push.dof_rolling;
+
+create table push.dof_rolling_count as
+select bbl, max(sale_date), count(*) as total
+from push.dof_rolling group by bbl;
+create index on push.dof_rolling_count(bbl);
+
+commit;
+
