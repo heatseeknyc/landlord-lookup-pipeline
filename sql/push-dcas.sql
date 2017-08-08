40bd8c8a1e609c64a8fb189d295613c786c18ab2
wstlabs <notyet@notyet.org>
2017-08-08 13:21:59 -0400

    add counting table

--- sql/push-dcas.sql
+++ sql/push-dcas.sql
@@ -10,8 +10,23 @@ create table push.dcas_ipis as select * from core.dcas_ipis;
 create index on push.dcas_ipis(bbl);
 
 create view push.dcas_ipis_tidy as
-select bbl, parcel_name, parcel_address, juris, agency, rpad, water, irreg, owntype, bldg_count renewal
+select bbl, name, address, juris, agency, rpad, water, irreg, owntype, bldg_count renewal
 from push.dcas_ipis;
+
+drop table if exists push.dcas_ipis_count cascade;
+create table push.dcas_ipis_count as
+select
+    bbl,
+    count(distinct name) as name,
+    count(distinct address) as address,
+    count(distinct juris) as juris,
+    count(distinct agency) as agency,
+    count(*) as total,
+    first(juris) as firstjuris,
+    first(name) firstname,
+    first(address) firstaddr
+from push.dcas_ipis group by bbl;
+create index on push.dcas_ipis_count(bbl);
 
 commit;
 
